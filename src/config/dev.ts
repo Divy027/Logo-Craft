c7c08a7ef183d8a30d9518a59860fe36279fab41
Divy <divyprajapati027@gmail.com>
2023-12-31 20:34:57 +0530

    blockhain chain ids

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -3,8 +3,14 @@ export const TOAST_TIME_OUT = 2000;
 export const INTERVAL = 6 * 1000;
 export const DECIMAL = 1000000000000000000;
 // export const CHAINID = '0x5' //Goerli
-export const CHAINID = "0x1b59"; // Zeta testnet
-// export const CHAINID = "0x13881"; //polygon testnet
+export const CHAINID = "0x1b59"; // HEX Zeta testnet
+//testnet
+export const Zeta_CHAINID = 7001;
+export const Matic_CHAINID = 80001; 
+export const Base_CHAINID = 84531;
+export const Bnb_CHAINID = 97;
+export const Avax_CHAINID = 43113;
+export const Eth_CHAINID = 11155111;
 
 export const WALLET_STATUS_LOCALSTORAGE = "wallet";
 export const WALLET_ADRESS_LOCALSTORAGE = "wallet_address";
