3a44e285d69e787b0c7bd13e18e79e0fa615acef
Divy <divyprajapati027@gmail.com>
2023-12-31 20:36:12 +0530

    remove wallet useEffect from Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import enter from "../assets/enter.png"
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { CONNECT } from "../actions";
-import { connectWallet, connectedChain, getBalance } from "../utils";
+import { connectWallet, getBalance } from "../utils";
 import CONFIG from "../config";
 import { ToastContainer,toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -25,18 +25,10 @@ const Home = () => {
     address: ``,
   });
   const [tokenAddress, setTokenAddress] = useState('');
-  const [blockchain, setBlockchain] = useState('');
+  const [blockchain, setBlockchain] = useState("Zeta");
   const [logoImage, setLogoImage] = useState(null); // File object
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [step, setStep] = useState(0);
   
-  const handleChangeTokenAddress = (event: any) => setTokenAddress(event.target.value);
-  const handleChangeBlockchain = (event: any) => setBlockchain(event.target.value);
-  const handleImageUpload = (event: any) => setLogoImage(event.target.files[0]);
-
-  const handleSubmit = async () => {
-    setIsSubmitting(true);
-  };
 
   const handleConnect = async () => {
     try {
@@ -66,99 +58,11 @@ const Home = () => {
     localStorage.setItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE, "");
   };
 
-  async function isConnected() {
-    const accounts = await window.ethereum.request({method: 'eth_accounts'});       
-    if (accounts.length) {
-       console.log(`You're connected to: ${accounts[0]}`);
-       dispatch(
-        CONNECT({
-          wallet: `connected`,
-          address: accounts[0],
-        })
-      );
-      localStorage.setItem(CONFIG.WALLET_STATUS_LOCALSTORAGE, "connected");
-      localStorage.setItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE, accounts[0]);
-    } else {
-       console.log("Metamask is not connected");
-       dispatch(
-        CONNECT({
-          wallet: `disconnect`,
-          address: ``,
-        })
-      );
-      localStorage.setItem(CONFIG.WALLET_STATUS_LOCALSTORAGE, "disconnect");
-      localStorage.setItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE, "");
-    }
- }
-
-  useEffect(() => {
-    const get_walletStatus: any = localStorage.getItem(CONFIG.WALLET_STATUS_LOCALSTORAGE);
-    const get_walletAddress: any = localStorage.getItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE);
-    setWalletStatus({
-      ...walletStatus,
-      status: get_walletStatus,
-      address: get_walletAddress,
-    });
-  }, [storeData]);
-
-  useEffect(()=> {
-    try{
-      if(!window.ethereum.isConnected() || !window.ethereum._metamask.isUnlocked()){
-        dispatch(
-          CONNECT({
-            wallet: `disconnect`,
-            address: ``,
-          })
-        );
-        localStorage.setItem(CONFIG.WALLET_STATUS_LOCALSTORAGE, "disconnect");
-        localStorage.setItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE, "");
-      }
-    }catch{
-
-    }
-  }, [])
-
-  useEffect(() => {
-    try {
-      window.ethereum.on("accountsChanged", async () => {
-        const wallet: any = await connectWallet();
-        localStorage.setItem(CONFIG.WALLET_STATUS_LOCALSTORAGE, "connected");
-        localStorage.setItem(CONFIG.WALLET_ADRESS_LOCALSTORAGE, wallet.address);
-        dispatch(
-          CONNECT({
-            wallet: "connected",
-            address: wallet.address,
-          })
-        );
-
-        setWalletStatus({
-          ...walletStatus,
-          status: "connected",
-          address: wallet.address,
-        });
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await isConnected();
-      } catch (error) {
-        console.error("Error fetching metamask:", error);
-        // Handle errors if needed
-      }
-    };
-
-    fetchData();
-  }, []);
-  
  
   return (
     <>
     <Header/>
+    <ToastContainer/>
     <div className="min-h-screen flex items-center justify-center">
       {step == 0 && (
         <div className="container mx-auto px-4">
@@ -241,7 +145,7 @@ const Home = () => {
       
 
       {step == 1 && (
-        <Form tokenAddress='' logoImage='' blockchain='' setStep={setStep}/>
+        <Form tokenAddress={tokenAddress} setTokenAddress = {setTokenAddress} blockchain={blockchain} setBlockchain = {setBlockchain} setStep={setStep}/>
       )}
 
       {step == 2 && (
